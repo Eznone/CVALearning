7ab14904024b4e93dfe0fca4ba51fe9b066f7542
Enzo <enzotresmediano@gmail.com>
2024-08-14 17:50:33 -0300

    Began learning moving images

---
+++ Chapter 6/6.1_translation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import imutils
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+cv2.waitKey(0)
+
+M = np.float32([[1, 0, 25], [0, 1, 50]])
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("Shifted Down and Right", shifted)
+cv2.waitKey(0)
+
+M = np.float32([[1, 0, -50], [0, 1, -90]])
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("Shifted Up and Left", shifted)
+cv2.waitKey(0)
