975da1c2a450257afc8e1c5993bbbacded726d55
Enzo <enzotresmediano@gmail.com>
2024-08-19 09:38:31 -0300

    Finished gray scale

---
+++ Chapter 7 Histograms/7.1_grayscale_histogram.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True)
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+# Convert the image to grayscale
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Original image", image)
+cv2.waitKey(0)
+
+# Creating Histogram
+hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+
+plt.figure()
+plt.title("GrayScale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("Number of pixels")
+plt.plot(hist)
+plt.xlim([0,256])
+plt.show()
+cv2.waitkey(0)
