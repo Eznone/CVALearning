9a1094c0b8c552c395fa488abebc11b2ef2398ae
Enzo <enzotresmediano@gmail.com>
2024-08-19 10:22:06 -0300

    Finished 2D histograms

--- Chapter 7 Histograms/7.3_double_channel_histograms.py
+++ Chapter 7 Histograms/7.3_double_channel_histograms.py
@@ -8,5 +8,29 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help = "Path of image required")
 args = vars(ap.parse_args())
 
+# Setting up the image
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
+chans = cv2.split(image)
+colors = ("b", "g", "r")
+
+# Plotting of image
+fig = plt.figure()
+ax = fig.add_subplot(131)
+hist = cv2.calcHist([chans[1], chans[0]], [0, 1], None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation= "nearest")
+ax.set_title("2D Color Histogram for G and B")
+plt.colorbar(p)
+
+ax = fig.add_subplot(132)
+hist = cv2.calcHist([chans[1], chans[2]], [0, 1], None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation= "nearest")
+ax.set_title("2D Color Histogram for G and R")
+plt.colorbar(p)
+
+ax = fig.add_subplot(133)
+hist = cv2.calcHist([chans[0], chans[2]], [0, 1], None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation= "nearest")
+ax.set_title("2D Color Histogram for B and R")
+plt.colorbar(p)
+plt.show()
